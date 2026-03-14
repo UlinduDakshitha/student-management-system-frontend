1ca01a696715a5a1e1a343e383e7fad14f3fe337
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-14 21:03:37 +0530

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,20 @@
+ import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-     
+    <div style={{ padding: "20px" }}>
+      <h1>Student Management System</h1>
+      <p>Welcome to the system</p>
+
+      <Link href="/login">
+        <button>Login</button>
+      </Link>
+
+      <br /><br />
+
+      <Link href="/register">
+        <button>Register</button>
+      </Link>
     </div>
   );
 }
