2c2844a0e68fc1a1de3c26044b3ef0f54d0b03f1
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-17 23:14:11 +0530

    update

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -79,6 +79,7 @@ export default function LoginPage() {
               />
             </label>
 
+
             <button type="submit" className={styles.submitButton}>
               Login
             </button>
